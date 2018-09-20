1ee42d787f2557ff5708d4926b30516e3531ed3f
willi-dev <willi.ilmukomputer@gmail.com>
2018-09-20 11:54:38 +0700

    setup configuration firebase key

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 var config = {
-  apiKey: "",
-  authDomain: "",
-  databaseUrl: "",
-  projectId: "",
-  storageBucket: "",
-  messangingSenderId: ""
+  apiKey: "AIzaSyAW1IpCG_6iBjABRMdEJc8dDSQp6ExWfis",
+  authDomain: "ss-product-catalog.firebaseapp.com",
+  databaseUrl: "https://ss-product-catalog.firebaseio.com",
+  projectId: "ss-product-catalog",
+  storageBucket: "ss-product-catalog.appspot.com",
+  messangingSenderId: "337100790331"
 }
 
 const firebaseConfig = firebase.initializeApp( config );
