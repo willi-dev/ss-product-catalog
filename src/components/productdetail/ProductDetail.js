16fd92b4b958a2550db8e8292a60e08a9b8736e8
willi-dev <willi.ilmukomputer@gmail.com>
2018-09-20 18:23:45 +0700

    add ProductList, add ProductDetail Component

---
+++ src/components/productdetail/ProductDetail.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ProductDetail extends Component {
+  
+  render() {
+    return (
+      <div className="ss-product__detail">
+
+      </div>
+    );
+  }
+
+}
+
+export default ProductDetail;
---
+++ src/components/productlist/ProductList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ProductList extends Component {
+
+  render() {
+    return (
+      <div className="ss-product__list">
+
+      </div>
+    );
+  }
+
+}
+
+export default ProductList;
