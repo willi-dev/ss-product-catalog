cc4b7cb451e31b0b898dc330d6d81523fa0b7682
willi-dev <willi.ilmukomputer@gmail.com>
2018-09-22 23:52:19 +0700

    add features cms

---
+++ src/components/auth/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+
+	render() {
+		return (
+			<div className="ss-login">
+
+			</div>
+		);
+	}
+	
+}
+
+export default Login;
